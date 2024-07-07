c7ca24e26a9a2bc9775a2199b8d62592d43ba4cb
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-07-07 11:49:34 -0700

    Delete

--- Supabase/src/actions/getUserData.ts
+++
@@ -1,6 +0,0 @@
-"use server";
-import { User } from "@/lib/types";
-
-const getUserData = async (): Promise<User | null | any> => { };
-
-export default getUserData;
--- Supabase/src/lib/types.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    id: string;
-    full_name: string;
-    avatar_url: string;
-    email: string;
-    job_title: string;
-    logo: string;
-};
